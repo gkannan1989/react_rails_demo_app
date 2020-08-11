1a5d88581bf513b59a4b094d5896f1f2a9646187
Kannan Ganesan <40201213+gkannan1989@users.noreply.github.com>
2020-08-11 21:55:20 +0100

    update on the event_tyoe

--- app/javascript/components/MultiselectTree.jsx
+++ app/javascript/components/MultiselectTree.jsx
@@ -1,23 +1,49 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import DropdownTreeSelect from 'react-dropdown-tree-select'
 import 'react-dropdown-tree-select/dist/styles.css'
 
-class MultiselectTree extends React.Component  {
-
-  onChange(currentNode, selectedNodes) {
-    console.log('onChange::', currentNode, selectedNodes)
+const Multiselect = ({data, setData}) => {
+  const onChange = (currentNode, selectedNodes) => {
+    setData(selectedNodes);
   }
-
-  render() {
-    return (
-      <React.Fragment>
-        <DropdownTreeSelect
-          data={this.props.data}
-          onChange={this.onChange}
-         />
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+       <DropdownTreeSelect
+        keepTreeOnSearch
+        data={data}
+        onChange={onChange}
+       />
+    </React.Fragment>
+  );
 }
+
+const HiddenNodes = ({selectedNodes}) => {
+  return (
+    <>
+       <input
+       type="hidden"
+       name={"event_type"}
+       value={JSON.stringify(selectedNodes)} /> 
+    </>
+  )
+}
+
+const renderNode = (selected) => {
+  return ReactDOM.render(<HiddenNodes selectedNodes={selected}/>, document.getElementById('hiddenNode'));
+}
+
+const MultiselectTree = ({ data }) => {
+  const getData = (res) => {
+    return renderNode(res)
+  }
+  return (
+    <React.Fragment>
+       <Multiselect data={data} setData={getData}/>
+       <div id={"hiddenNode"} />
+    </React.Fragment>
+  );
+}
+
 
 export default MultiselectTree
